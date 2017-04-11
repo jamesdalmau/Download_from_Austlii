9e28ac0be03ed82ae760451719defb8389d1c805
mafiastewart <jamesdalmau@gmail.com>
2017-04-11 22:16:08 +1000

    Revision of FindAuthorsNoTagText - about half solved

--- src/FindAuthorsNoTagText.py
+++ src/FindAuthorsNoTagText.py
@@ -1,11 +1,19 @@
 file_to_write = open('authors_no_tag_text_results.txt', 'w', encoding="utf8", errors="replace")
 import os
 
-with open('missingauthorsNewMethodRequired.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
+with open('authors_no_tag_text.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
     lines_in_files_to_read = list_of_files_to_read.readlines()
     for file_in_list in lines_in_files_to_read:
         with open('d:\\txtutf\\' + file_in_list.strip(), encoding="utf8", errors='ignore') as f:
             lines_found = ''
-            #print(file)
             lines = f.readlines()
+            line_number = 0
             for line in lines:
+                line_number = line_number + 1
+                if line_number < 100:
+                    uppercase_line = line.upper()
+                    uppercase_line_stripped = uppercase_line.strip()
+                    if len(uppercase_line_stripped) > 2:
+                        if uppercase_line_stripped[-1]=='J' or uppercase_line_stripped[-2]=='J.':
+                            lines_found = lines_found + '|' + uppercase_line.strip()
+            file_to_write.write(file_in_list.strip() + lines_found + "\n")
