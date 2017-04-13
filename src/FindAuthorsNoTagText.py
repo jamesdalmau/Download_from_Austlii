889f933d7a8db3620064dff954499b4a14994015
mafiastewart <jamesdalmau@gmail.com>
2017-04-13 22:45:01 +1000

    Finished fiddling with and using FindAuthors and FindAuthorsNoTagText (65 judgments not traceable)

--- src/FindAuthorsNoTagText.py
+++ src/FindAuthorsNoTagText.py
@@ -1,5 +1,6 @@
 file_to_write = open('authors_no_tag_text_results.txt', 'w', encoding="utf8", errors="replace")
 import os
+list_of_possible_tags = ['JUDGMENT OF THE FULL COURT','JUDGMENT OF THE COURT OF CRIMINAL APPEAL']
 
 with open('authors_no_tag_text.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
     lines_in_files_to_read = list_of_files_to_read.readlines()
@@ -8,12 +9,16 @@ with open('authors_no_tag_text.txt', 'r', encoding="utf8", errors="replace") as 
             lines_found = ''
             lines = f.readlines()
             line_number = 0
+            possible_tag_found = 0
             for line in lines:
                 line_number = line_number + 1
-                if line_number < 100:
+                if line_number < 30:
+                    regularcase_line_stripped = line.strip()
                     uppercase_line = line.upper()
                     uppercase_line_stripped = uppercase_line.strip()
-                    if len(uppercase_line_stripped) > 2:
-                        if uppercase_line_stripped[-1]=='J' or uppercase_line_stripped[-2]=='J.':
-                            lines_found = lines_found + '|' + uppercase_line.strip()
+                    if possible_tag_found == 1:
+                        lines_found = lines_found + '|' + uppercase_line.strip()
+                        possible_tag_found = 0
+                    if "REASONS FOR JUDGMENT" in uppercase_line_stripped:
+                        possible_tag_found = 1
             file_to_write.write(file_in_list.strip() + lines_found + "\n")
