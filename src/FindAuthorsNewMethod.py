8dfd0ecdfd3b1d8b0268953005c4b5a3e9f88440
mafiastewart <jamesdalmau@gmail.com>
2017-04-15 15:29:33 +1000

    Found some suss WA files, trying to filter more full court judgments

--- src/FindAuthorsNewMethod.py
+++ src/FindAuthorsNewMethod.py
@@ -1,9 +1,9 @@
-file_to_write = open('finalmissingauthorsfound.txt', 'w', encoding="utf8", errors="replace")
+file_to_write = open('title_of_court_results.txt', 'w', encoding="utf8", errors="replace")
 import os
-list_of_possible_tags = ['JUDGMENT OF :', 'COURT:	']
-list_of_excluded_tags = ['(APPEALS TO A SINGLE JUDGE: APPLICATION)', '(APPEALS TO A SINGLE JUDGE: CIVIL)', 'APPEAL TO A SINGLE JUDGE: CIVIL', '(APPEALS TO A SINGLE JUDGE: CRIMINAL)', '(APPEALS TO A SINGLE JUDGE: PERMISSION TO APPEAL)', '(COURT OF CRIMINAL APPEAL: CRIMINAL)', '<LCCORAM>', 'APPEAL FROM A MASTER: CIVIL', 'APPEAL FROM A MASTER: APPLICATION']
+list_of_possible_tags = ['<TITLEOFCOURT>']
+list_of_excluded_tags = ['(APPEALS TO A SINGLE JUDGE: APPLICATION)']
 
-with open('missingauthorsNewMethodRequired.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
+with open('title_of_court_files.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
     lines_in_files_to_read = list_of_files_to_read.readlines()
     for file_in_list in lines_in_files_to_read:
         with open('d:\\txtutf\\' + file_in_list.strip(), encoding="utf8", errors='ignore') as f:
--- src/FindAuthorsNoTagText.py
+++ src/FindAuthorsNoTagText.py
@@ -1,6 +1,6 @@
 file_to_write = open('authors_no_tag_text_results.txt', 'w', encoding="utf8", errors="replace")
 import os
-list_of_possible_tags = ['JUDGMENT OF THE FULL COURT','JUDGMENT OF THE COURT OF CRIMINAL APPEAL']
+list_of_possible_tags = ['APPEAL', 'FULL']
 
 with open('authors_no_tag_text.txt', 'r', encoding="utf8", errors="replace") as list_of_files_to_read:
     lines_in_files_to_read = list_of_files_to_read.readlines()
@@ -16,9 +16,11 @@ with open('authors_no_tag_text.txt', 'r', encoding="utf8", errors="replace") as 
                     regularcase_line_stripped = line.strip()
                     uppercase_line = line.upper()
                     uppercase_line_stripped = uppercase_line.strip()
-                    if possible_tag_found == 1:
-                        lines_found = lines_found + '|' + uppercase_line.strip()
-                        possible_tag_found = 0
-                    if "REASONS FOR JUDGMENT" in uppercase_line_stripped:
-                        possible_tag_found = 1
+                    for possible_tag in list_of_possible_tags:
+                        if possible_tag in uppercase_line_stripped:
+                    #if possible_tag_found == 1:
+                            lines_found = lines_found + '|' + uppercase_line.strip()
+                        #possible_tag_found = 0
+                    #if "REASONS FOR JUDGMENT" in uppercase_line_stripped:
+                        #possible_tag_found = 1
             file_to_write.write(file_in_list.strip() + lines_found + "\n")
